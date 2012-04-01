1801a398a5a0de5864b3fe8659ab81481bef9ece
Jocelyn Kong <jocelynk@andrew.cmu.edu>
2012-04-01 13:59:30 -0400

    xml running

--- gen/com/team03/fitsup/R.java
+++ gen/com/team03/fitsup/R.java
@@ -14,28 +14,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int ic_tab_artists_grey=0x7f020001;
         public static final int ic_tab_artists_white=0x7f020002;
-        public static final int tabs_workouts=0x7f020003;
+        public static final int running=0x7f020003;
+        public static final int tabs_workouts=0x7f020004;
     }
     public static final class id {
         public static final int confirm=0x7f060002;
         public static final int date=0x7f060012;
         public static final int dateDisplay=0x7f060004;
-        public static final int description=0x7f060014;
+        public static final int description=0x7f06001a;
+        public static final int disance=0x7f060018;
         public static final int distance=0x7f06000d;
         public static final int hours=0x7f060008;
         public static final int hr=0x7f060007;
-        public static final int menu_delete_r=0x7f060017;
-        public static final int menu_delete_wr=0x7f06001a;
-        public static final int menu_delete_wre=0x7f06001c;
-        public static final int menu_edit_r=0x7f060016;
-        public static final int menu_edit_wr=0x7f060019;
-        public static final int menu_view_r=0x7f060015;
-        public static final int menu_view_wr=0x7f060018;
-        public static final int menu_view_wre=0x7f06001b;
+        public static final int imageButton1=0x7f060013;
+        public static final int linearLayout1=0x7f060015;
+        public static final int menu_delete_r=0x7f06001d;
+        public static final int menu_delete_wr=0x7f060020;
+        public static final int menu_delete_wre=0x7f060022;
+        public static final int menu_edit_r=0x7f06001c;
+        public static final int menu_edit_wr=0x7f06001f;
+        public static final int menu_view_r=0x7f06001b;
+        public static final int menu_view_wr=0x7f06001e;
+        public static final int menu_view_wre=0x7f060021;
+        public static final int mi=0x7f060016;
         public static final int miles=0x7f06000f;
         public static final int min=0x7f060009;
         public static final int minutes=0x7f06000a;
-        public static final int name=0x7f060013;
+        public static final int name=0x7f060019;
         public static final int name1=0x7f060006;
         public static final int pickDate=0x7f060005;
         public static final int sec=0x7f06000b;
@@ -43,6 +48,8 @@ public final class R {
         public static final int spinner1=0x7f060000;
         public static final int spinner2=0x7f060001;
         public static final int text1=0x7f060003;
+        public static final int textView6=0x7f060014;
+        public static final int time=0x7f060017;
         public static final int value=0x7f06000e;
         public static final int value2=0x7f060010;
         public static final int value3=0x7f060011;
@@ -56,10 +63,11 @@ public final class R {
         public static final int records_row=0x7f030005;
         public static final int records_view=0x7f030006;
         public static final int records_view2=0x7f030007;
-        public static final int workouts_edit=0x7f030008;
-        public static final int workouts_index=0x7f030009;
-        public static final int workouts_options=0x7f03000a;
-        public static final int workouts_row=0x7f03000b;
+        public static final int test=0x7f030008;
+        public static final int workouts_edit=0x7f030009;
+        public static final int workouts_index=0x7f03000a;
+        public static final int workouts_options=0x7f03000b;
+        public static final int workouts_row=0x7f03000c;
     }
     public static final class menu {
         public static final int record_context_menu=0x7f050000;
